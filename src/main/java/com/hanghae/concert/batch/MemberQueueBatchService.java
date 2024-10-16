e48b1cbf5ea1f40d025240a48a0e26d5a7efdc8e
whee <hwiveloper17@naver.com>
2024-10-16 20:16:17 +0900

    [default] 대기열 스케쥴러 - 만료, active 변경(미구현)

---
+++ src/main/java/com/hanghae/concert/batch/MemberQueueBatchService.java
@@ -0,0 +1,9 @@
+package com.hanghae.concert.batch;
+
+import lombok.*;
+import org.springframework.stereotype.*;
+
+@RequiredArgsConstructor
+@Service
+public class MemberQueueBatchService {
+}
---
+++ src/main/java/com/hanghae/concert/batch/task/MemberQueueTask.java
@@ -0,0 +1,21 @@
+package com.hanghae.concert.batch.task;
+
+import com.hanghae.concert.batch.*;
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.springframework.scheduling.annotation.*;
+import org.springframework.stereotype.*;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MemberQueueTask {
+
+    private final MemberQueueBatchService memberQueueBatchService;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void changeTokenStatusActiveToExpired() {
+
+//        memberQueueBatchService.();
+    }
+}
